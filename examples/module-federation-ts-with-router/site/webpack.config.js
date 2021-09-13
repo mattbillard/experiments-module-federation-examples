29bf7a0c6458a5f6e99975f5a5ee2dd163b62716
Matt Billard <2836258+mattbillard@users.noreply.github.com>
2021-09-13 10:22:15 -0400

    WIP - rename app1 to site. Duplicte app2 as app1

--- examples/module-federation-ts-with-router/site/webpack.config.js
+++ examples/module-federation-ts-with-router/site/webpack.config.js
@@ -5,22 +5,35 @@ const path = require("path");
 module.exports = {
   mode: "development",
 
-  // // NOTE: if you wanted to run this app independently on its own port you could uncomment the following and change the start script back to "webpack-cli serve"
+  // NOTE: if you wanted to run this app independently on its own port you could uncomment the following and change the start script back to "webpack-cli serve"
   devServer: {
-    port: 3002,
+    port: 3001,
     static: {
       directory: path.join(__dirname, "dist"),
     },
-    hot: false,
-    devMiddleware: {
-      writeToDisk: true,
+    historyApiFallback: {
+      index: '/assets/app1/index.html'  // NOTE: router needs a fallback if app1 hasn't been built. TODO
     },
+    hot: false,
+    // devMiddleware: {
+    //   writeToDisk: true,
+    // },
+    proxy: [
+      {
+        changeOrigin: true,
+        context: ['/assets/app2'],
+        cookieDomainRewrite: 'localhost',
+        secure: false,
+        target: 'http://localhost:3002', // NOTE: requests at /app2/ are proxied to localhost:3002
+        ws: true,
+      }
+    ],
   },
 
   devtool: 'source-map',
   entry: "./src/index",
   output: {
-    publicPath: "/assets/app2/",
+    publicPath: "/assets/app1/",
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js"],
@@ -43,10 +56,9 @@ module.exports = {
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "app2",
-      filename: "remoteEntry.js",
-      exposes: {
-        "./button": "./src/components/button/button",
+      name: "app1",
+      remotes: {
+        app2: "app2@/assets/app2/remoteEntry.js", // NOTE: find app2 at /assets/app2/ instead of localhost:3002
       },
       shared: ["react", "react-dom"],
     }),
