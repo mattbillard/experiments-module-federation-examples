916a751f72897e47cff08eed186e7c98db79036b
Matt Billard <2836258+mattbillard@users.noreply.github.com>
2021-09-13 17:37:04 -0400

    WIP - allow local definitions

--- examples/module-federation-ts-with-router/site/src/components/app/app.tsx
+++ examples/module-federation-ts-with-router/site/src/components/app/app.tsx
@@ -1,36 +1,75 @@
+import * as _ from 'lodash';
 import * as React from "react";
-import { BrowserRouter, Link, Route, Switch, RouteProps } from "react-router-dom";
+import { useEffect, useState } from "react";
+import {
+  BrowserRouter,
+  Link,
+  Route,
+  Switch,
+  RouteProps,
+} from "react-router-dom";
 import LocalButton from "../button/button";
 
-// @ts-ignore
-const definitions = require('./micro-app-definitions.json');
+const defaultDefitions = require("./micro-app-definitions.json");
 
 // TODO: fix types
 const RemoteButton1 = React.lazy(() => import("app1/button"));
 const RemoteButton2 = React.lazy(() => import("app2/button"));
 
-const App = () => (
-  <BrowserRouter>
-    <div>
-      <h1>module-federation-ts-with-router</h1>
-      <h2>Site</h2>
-      
-      <LocalButton /> 
-      <br /><br />
+const App = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [definitions, setDefinitions] = useState(defaultDefitions);
 
+  useEffect(() => {
+    (async () => {
+      // Allow overwrite of defaultDefitions with local definitions
+      try {
+        const res = await fetch('/public/test.json');
+        const data = await res.json();
+        const newDefinitions = _.merge({}, defaultDefitions, data);
+        setDefinitions(newDefinitions);
+      } catch (error) {
+        // noop
+      }
+
+      setIsLoading(false);
+    })();
+  });
+
+  if (isLoading) {
+    return null;
+  }
+
+  return (
+    <BrowserRouter>
       <div>
-        <Link to={definitions.nav.App1}>App1</Link> | 
-        <Link to={definitions.nav.App2}>App2</Link>
-      </div>
+        <h1>module-federation-ts-with-router</h1>
+        <h2>Site</h2>
 
-      <React.Suspense fallback="Loading...">
-        <Switch>
-          <Route path={definitions.nav.App1} render={(routeProps: RouteProps) => <RemoteButton1 />} />
-          <Route path={definitions.nav.App2} render={(routeProps: RouteProps) => <RemoteButton2 />} />
-        </Switch>
-      </React.Suspense>
-    </div>
-  </BrowserRouter>
-);
+        <LocalButton />
+        <br />
+        <br />
+
+        <div>
+          <Link to={definitions.nav.App1}>App1</Link> |
+          <Link to={definitions.nav.App2}>App2</Link>
+        </div>
+
+        <React.Suspense fallback="Loading...">
+          <Switch>
+            <Route
+              path={definitions.nav.App1}
+              render={(routeProps: RouteProps) => <RemoteButton1 />}
+            />
+            <Route
+              path={definitions.nav.App2}
+              render={(routeProps: RouteProps) => <RemoteButton2 />}
+            />
+          </Switch>
+        </React.Suspense>
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
