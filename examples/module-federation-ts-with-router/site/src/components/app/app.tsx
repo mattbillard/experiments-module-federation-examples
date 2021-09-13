b8c1f5ece1c8eb6da239bd3bab4b45c1b93ac7a9
Matt Billard <2836258+mattbillard@users.noreply.github.com>
2021-09-13 16:29:53 -0400

    WIP - nav definition

--- examples/module-federation-ts-with-router/site/src/components/app/app.tsx
+++ examples/module-federation-ts-with-router/site/src/components/app/app.tsx
@@ -2,6 +2,9 @@ import * as React from "react";
 import { BrowserRouter, Link, Route, Switch, RouteProps } from "react-router-dom";
 import LocalButton from "../button/button";
 
+// @ts-ignore
+const definitions = require('./micro-app-definitions.json');
+
 // TODO: fix types
 const RemoteButton1 = React.lazy(() => import("app1/button"));
 const RemoteButton2 = React.lazy(() => import("app2/button"));
@@ -16,14 +19,14 @@ const App = () => (
       <br /><br />
 
       <div>
-        <Link to="/site/app1">App1</Link> | 
-        <Link to="/site/app2">App2</Link>
+        <Link to={definitions.nav.App1}>App1</Link> | 
+        <Link to={definitions.nav.App2}>App2</Link>
       </div>
 
       <React.Suspense fallback="Loading...">
         <Switch>
-          <Route path="/site/app1" render={(routeProps: RouteProps) => <RemoteButton1 />} />
-          <Route path="/site/app2" render={(routeProps: RouteProps) => <RemoteButton2 />} />
+          <Route path={definitions.nav.App1} render={(routeProps: RouteProps) => <RemoteButton1 />} />
+          <Route path={definitions.nav.App2} render={(routeProps: RouteProps) => <RemoteButton2 />} />
         </Switch>
       </React.Suspense>
     </div>
