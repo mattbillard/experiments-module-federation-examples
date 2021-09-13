7528f746c6653d13c99f6158e6913c6bad569e45
Matt Billard <2836258+mattbillard@users.noreply.github.com>
2021-09-13 15:03:09 -0400

    WIP - router is now site-launcher

---
+++ examples/module-federation-ts-with-router/site-launcher/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+
+const proxiesHashMap = {
+  // '/assets/site': 'http://localhost:1000',
+  // '/site':        'http://localhost:1000',
+  '/assets/app1': 'http://localhost:1001',
+  // '/assets/app2': 'http://localhost:1002',
+
+  // Anything not local will be proxied from dev-mock
+  '/':            'http://localhost:3000',
+};
+
+const proxy = Object.entries(proxiesHashMap).map(([key, value]) => {
+  return {
+    changeOrigin: true,
+    context: [key],
+    cookieDomainRewrite: 'localhost',
+    secure: false,
+    target: value,
+    ws: true,
+  }
+});
+
+module.exports = (env = {}) => {
+  return {
+    mode: "development",
+    devServer: {
+      port: 2000,
+      host: '0.0.0.0',
+      hot: false,
+      liveReload: false,
+      proxy,
+      static: {
+        directory: path.join(__dirname, "dist"),
+      },
+    },
+  };
+};
