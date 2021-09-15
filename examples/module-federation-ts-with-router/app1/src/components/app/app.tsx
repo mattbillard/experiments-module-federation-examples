a826f1e5797b4708c815b5222eb6d7c62944f236
Matt Billard <2836258+mattbillard@users.noreply.github.com>
2021-09-15 14:06:18 -0400

    WIP - nested router

--- examples/module-federation-ts-with-router/app1/src/components/app/app.tsx
+++ examples/module-federation-ts-with-router/app1/src/components/app/app.tsx
@@ -1,4 +1,11 @@
 import * as React from "react";
+import {
+  BrowserRouter,
+  Link,
+  Route,
+  Switch,
+  RouteProps,
+} from "react-router-dom";
 import LocalButton from "../button/button";
 
 const App = () => (
@@ -6,6 +13,18 @@ const App = () => (
     <h1>module-federation-ts-with-router</h1>
     <h2>App 1</h2>
     <LocalButton />
+
+    <BrowserRouter>
+      <div>
+        <Link to="/site/app1app/test1">Test 1</Link> | 
+        <Link to="/site/app1app/test2">Test 2</Link> | 
+      </div>
+
+      <Switch>
+        <Route path="/site/app1app/test1">Test 1</Route>
+        <Route path="/site/app1app/test2">Test 2</Route>
+      </Switch>
+    </BrowserRouter>
   </div>
 );
 
